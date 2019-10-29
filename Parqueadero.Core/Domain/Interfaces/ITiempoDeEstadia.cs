97de3eebf673aab9565dbf7822e09ad9d430f672
Felipe Arcos <pipearcos221@gmail.com>
2019-10-29 15:07:25 -0500

    Se crea la interfaz relacionada al tiempo de estadia de un vehiculo en el parqueadero

---
+++ Parqueadero.Core/Domain/Interfaces/ITiempoDeEstadia.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Parqueadero.Core.Domain.Interfaces
+{
+    public interface ITiempoDeEstadia
+    {
+        int ObtenerNumeroDeDiasDeEstadia(DateTime fechaIngreso);
+
+        int ObtenerNumeroDeHorasDeEstadia(DateTime fechaIngreso);
+    }
+}
