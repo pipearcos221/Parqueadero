cd5184deb9cf1159f018a999f770bd2e81575319
Felipe Arcos <pipearcos221@gmail.com>
2019-10-29 08:43:48 -0500

    Se crea la clase Vehiculo

---
+++ Parqueadero.Core/Domain/Vehiculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Parqueadero.Core.Domain
+{
+    public class Vehiculo
+    {
+        public string Tipo { get; set; }
+        public string Cilindraje { get; set; }
+        public string Placa { get; set; }
+        public DateTime FechaIngreso { get; set; }
+
+        public Vehiculo(string tipo, string cilindraje, string placa, DateTime fechaIngreso)
+        {
+            Tipo = tipo;
+            Cilindraje = cilindraje;
+            Placa = placa;
+            FechaIngreso = fechaIngreso;
+        }
+    }
+}
