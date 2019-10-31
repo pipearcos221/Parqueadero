4d87660daf7c126a1d22124fced1e3b920d0a433
Felipe Arcos <pipearcos221@gmail.com>
2019-10-31 08:55:13 -0500

    Se crea la implementacion de la interfaz Facturacion

---
+++ Parqueadero.Core/Domain/FacturacionImpl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Parqueadero.Core.Domain.Interfaces;
+
+namespace Parqueadero.Core.Domain
+{
+    public class FacturacionImpl : IFacturacion
+    {
+        public int CalcularPrecioAPagar(int numeroDias, int numeroHoras, int tipoDeVehiculo, int cilindraje)
+        {
+            int valorAPagar;
+            if (tipoDeVehiculo.Equals(Constantes.TIPO_CARRO))
+            {
+                valorAPagar = numeroDias * Constantes.valorDiaCarro + numeroHoras * Constantes.valorHoraCarro;
+            }
+            else
+            {
+                valorAPagar = numeroDias * Constantes.valorDiaMoto + numeroHoras * Constantes.valorHoraMoto;
+                if (cilindraje >= Constantes.cilindrajeParaExcedente) valorAPagar += Constantes.valorExcedenteMoto;
+            }
+            return valorAPagar;
+        }
+    }
+}
