7e9b93852c78b35a3287bc2b9fc1826bb3b2ecda
Felipe Arcos <pipearcos221@gmail.com>
2019-10-31 08:58:47 -0500

    Se crean las pruebas unitarias para la disponibilidad de espacio en el parqueadero

---
+++ Parqueadero.Core.Test/DisponibilidadDeEspacioTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Parqueadero.Core.Domain.Interfaces;
+using Xunit;
+namespace Parqueadero.Core.Test
+{
+    public class DisponibilidadDeEspacioTest
+    {
+        [Fact]
+        public void VerificarDisponibilidadDeEspacioDeParqueoTest()
+        {
+            // arrange
+            DisponibilidadDeEspacioImpl disponibilidad = new DisponibilidadDeEspacioImpl();
+            // act
+            bool respuesta = disponibilidad.VerificarDisponibilidadDeEspacioDeParqueo(19, 10, 1);
+            // assert
+            Assert.True(respuesta);
+        }
+    }
+}
