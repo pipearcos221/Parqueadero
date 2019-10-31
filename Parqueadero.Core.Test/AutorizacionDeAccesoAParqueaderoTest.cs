36c51790e0e6c2f26f26e31479e416bc95cec25d
Felipe Arcos <pipearcos221@gmail.com>
2019-10-31 08:58:23 -0500

    Se crean las pruebas unitarias para la autorización de acceso al parqueadero

---
+++ Parqueadero.Core.Test/AutorizacionDeAccesoAParqueaderoTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Parqueadero.Core.Domain;
+using Xunit;
+
+namespace Parqueadero.Core.Test
+{
+    public class AutorizacionDeAccesoAParqueaderoTest
+    {
+        [Fact]
+        public void ValidarAutorizacionParaAccederAlParqueaderoTest()
+        {
+            // arrange
+            AutorizacionDeAccesoAParqueaderoImpl autorizacion = new AutorizacionDeAccesoAParqueaderoImpl();
+            // act
+            bool respuesta = autorizacion.ValidarAutorizacionParaAccederAlParqueadero("DFQ566");
+            // assert
+            Assert.True(respuesta);
+        }
+    }
+}
