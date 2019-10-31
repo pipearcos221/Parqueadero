930f0f500cff77747a8d11725e90644f63643c41
Felipe Arcos <pipearcos221@gmail.com>
2019-10-31 09:01:39 -0500

    Se crean las pruebas unitarias para la facturacion

---
+++ Parqueadero.Core.Test/FacturacionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Parqueadero.Core.Domain;
+using Xunit;
+
+namespace Parqueadero.Core.Test
+{
+    public class FacturacionTest
+    {
+        [Fact]
+        public void CalcularPrecioAPagarTest() {
+            // arrange
+            FacturacionImpl facturacion = new FacturacionImpl();
+            // act
+            int precioAPagar = facturacion.CalcularPrecioAPagar(1, 1, 1, 1600);
+            // assert
+            Assert.Equal(9000, precioAPagar);
+        }
+    }
+}
