a35c60c96936cc61a21bda4de70b443696b53e68
Felipe Arcos <pipearcos221@gmail.com>
2019-10-31 11:00:37 -0500

    refactor para mitigar warnings

--- Parqueadero.Core/Domain/Constantes.cs
+++ Parqueadero.Core/Domain/Constantes.cs
@@ -3,20 +3,20 @@ namespace Parqueadero.Core.Domain
 {
     public static class Constantes
     {
-        public const int TIPO_CARRO = 1;
-        public const int TIPO_MOTO = 2;
+        public static readonly int TIPO_CARRO = 1;
+        public static readonly int TIPO_MOTO = 2;
 
-        public const int limiteDeCarros = 20;
-        public const int limiteDeMotos = 10;
+        public static readonly int limiteDeCarros = 20;
+        public static readonly int limiteDeMotos = 10;
 
-        public const int valorHoraCarro = 1000;
-        public const int valorHoraMoto = 500;
-        public const int valorDiaCarro = 8000;
-        public const int valorDiaMoto = 4000;
-        public const int valorExcedenteMoto = 2000;
+        public static readonly int valorHoraCarro = 1000;
+        public static readonly int valorHoraMoto = 500;
+        public static readonly int valorDiaCarro = 8000;
+        public static readonly int valorDiaMoto = 4000;
+        public static readonly int valorExcedenteMoto = 2000;
 
-        public const int cilindrajeParaExcedente = 500;
+        public static readonly int cilindrajeParaExcedente = 500;
 
-        public const int limiteDeCobroPorHoras = 9;
+        public static readonly int limiteDeCobroPorHoras = 9;
     }
 }
