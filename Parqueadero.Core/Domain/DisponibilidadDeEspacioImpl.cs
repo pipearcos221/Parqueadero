5ca5f1a6342d3fbe41948ae1293f8396ded7c962
Felipe Arcos <pipearcos221@gmail.com>
2019-10-31 08:54:54 -0500

    Se crea la implementación de la interfaz para verificar la disponibilidad de espacio dentro del parqueadero

---
+++ Parqueadero.Core/Domain/DisponibilidadDeEspacioImpl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Parqueadero.Core.Domain.Interfaces
+{
+    public class DisponibilidadDeEspacioImpl : IDisponibilidadDeEspacio
+    {
+        public bool VerificarDisponibilidadDeEspacioDeParqueo(int numeroCarrosEnParqueadero, int numeroMotosEnParqueadero, int tipoDeVehiculo)
+        {
+            if (tipoDeVehiculo.Equals(Constantes.TIPO_CARRO) && numeroCarrosEnParqueadero >= Constantes.limiteDeCarros
+                || tipoDeVehiculo.Equals(Constantes.TIPO_MOTO) && numeroMotosEnParqueadero >= Constantes.limiteDeMotos) return false;
+            return true;
+        }
+    }
+}
