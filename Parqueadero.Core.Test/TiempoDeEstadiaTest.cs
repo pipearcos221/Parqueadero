9c663fb6df75a017903ccd47a4ba6b4d53c3e4d4
Felipe Arcos <pipearcos221@gmail.com>
2019-10-31 09:01:24 -0500

    Se crean las pruebas unitarias para el tiempo de estadia

---
+++ Parqueadero.Core.Test/TiempoDeEstadiaTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Parqueadero.Core.Domain.Interfaces;
+using Xunit;
+
+namespace Parqueadero.Core.Test
+{
+    public class TiempoDeEstadiaTest
+    {
+        [Fact]
+        public void ObtenerNumeroDeDiasDeEstadiaTest()
+        {
+            // arrange
+            DateTime fechaIngreso = DateTime.Now.AddDays(-2);
+            TiempoDeEstadiaImpl tiempoDeEstadia = new TiempoDeEstadiaImpl();
+            // act
+            int diasACobrar = tiempoDeEstadia.ObtenerNumeroDeDiasDeEstadia(fechaIngreso);
+            // assert
+            Assert.Equal(2, diasACobrar);
+        }
+
+        [Fact]
+        public void ObtenerNumeroDeHorasDeEstadiaTest()
+        {
+            // arrange
+            DateTime fechaIngreso = DateTime.Now.AddDays(-2).AddHours(-8);
+            TiempoDeEstadiaImpl tiempoDeEstadia = new TiempoDeEstadiaImpl();
+            // act
+            int horasACobrar = tiempoDeEstadia.ObtenerNumeroDeHorasDeEstadia(fechaIngreso);
+            // assert
+            Assert.Equal(8, horasACobrar);
+        }
+    }
+}
