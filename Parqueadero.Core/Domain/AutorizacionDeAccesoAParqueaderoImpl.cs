d5af4c9769a686809ee7fc774f0bf836674a6455
Felipe Arcos <pipearcos221@gmail.com>
2019-10-31 08:54:12 -0500

    Se crea la implementación de la interfaz de autorización de acceso al parqueadero

---
+++ Parqueadero.Core/Domain/AutorizacionDeAccesoAParqueaderoImpl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Parqueadero.Core.Domain.Interfaces;
+
+namespace Parqueadero.Core.Domain
+{
+    public class AutorizacionDeAccesoAParqueaderoImpl : IAutorizacionDeAccesoAParqueadero
+    {
+        public bool ValidarAutorizacionParaAccederAlParqueadero(string placa)
+        {
+            string primeraLetraPlaca = placa.ToUpper().Substring(0, 1);
+            DateTime fechaActual = DateTime.Now;
+            DateTimeFormatInfo dateTimeFormat = new CultureInfo("es-ES").DateTimeFormat;
+            string diaActual = fechaActual.ToString("dddd", dateTimeFormat).ToLower();
+            if (primeraLetraPlaca.Equals("A"))
+            {
+                if (diaActual.Equals("lunes") || diaActual.Equals("domingo")) return true;
+                else return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+    }
+}
