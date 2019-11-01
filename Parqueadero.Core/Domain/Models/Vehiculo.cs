d9db1e2c079d9c82af6a68f778eb361c6275147c
Felipe Arcos <pipearcos221@gmail.com>
2019-11-01 14:41:45 -0500

    se cambia el nombre de paquete Entity por Models

--- Parqueadero.Core/Domain/Models/Vehiculo.cs
+++ Parqueadero.Core/Domain/Models/Vehiculo.cs
@@ -3,12 +3,7 @@ namespace Parqueadero.Core.Domain
 {
     public class Vehiculo
     {
-        public int Tipo { get; set; }
-        public int Cilindraje { get; set; }
-        public string Placa { get; set; }
-        public DateTime FechaIngreso { get; set; }
 
-        public Vehiculo(int tipo, int cilindraje, string placa, DateTime fechaIngreso)
         {
             Tipo = tipo;
             Cilindraje = cilindraje;
