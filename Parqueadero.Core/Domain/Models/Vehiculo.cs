8d7de597a73d3a41cd824b5ccad754c251ddd82a
Felipe Arcos <pipearcos221@gmail.com>
2019-11-01 15:41:58 -0500

    se cambia el tipo de variable para la variable tipo de vehículo de int a vehicletypes

--- Parqueadero.Core/Domain/Models/Vehiculo.cs
+++ Parqueadero.Core/Domain/Models/Vehiculo.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using Parqueadero.Core.Domain.Enumerations;
+
 namespace Parqueadero.Core.Domain
 {
     public class Vehiculo
     {
+        public VehicleTypes Tipo { get; private set; }
+        public int Cilindraje { get; private set; }
+        public string Placa { get; private set; }
+        public DateTime FechaIngreso { get; private set; }
 
+        public Vehiculo(VehicleTypes tipo, int cilindraje, string placa, DateTime fechaIngreso)
         {
             Tipo = tipo;
             Cilindraje = cilindraje;
