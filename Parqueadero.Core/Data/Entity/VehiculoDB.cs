6220920fd0cee9d0d020e4b4136f1abfe80ddbcf
Felipe Arcos <pipearcos221@gmail.com>
2019-11-01 15:39:06 -0500

    Se añade la entidad vehiculo que se va a mapear en realm

---
+++ Parqueadero.Core/Data/Entity/VehiculoDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Realms;
+
+namespace Parqueadero.Core.Data.Entity
+{
+    public class VehiculoDB : RealmObject
+    {
+        [PrimaryKey]
+        public long Id { get; set; } = Convert.ToInt64(Guid.NewGuid().ToString());
+        public int Tipo { get; set; }
+        public int Cilindraje { get; set; }
+        public string Placa { get; set; }
+        public string FechaIngreso { get; set; }
+    }
+}
