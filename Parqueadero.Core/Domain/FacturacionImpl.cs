e26f50ba605b07abb38bd1a5ec1e3126a7fe0cfb
Felipe Arcos <pipearcos221@gmail.com>
2019-11-05 15:50:15 -0500

    Se elimina import de paquete no usado

--- Parqueadero.Core/Domain/FacturacionImpl.cs
+++ Parqueadero.Core/Domain/FacturacionImpl.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿                                                                                                                                                                     using System;
 using Parqueadero.Core.Domain.Enumerations;
 using Parqueadero.Core.Domain.Interfaces;
 
