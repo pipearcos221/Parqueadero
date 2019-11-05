1e695fa9e390d532d266531e6c6b8483b003bddd
Felipe Arcos <pipearcos221@gmail.com>
2019-11-05 10:28:33 -0500

    se modifica el modelo Vehiculo, modificando el Tipo de variable del atributo Tipo

--- Parqueadero.Core/Domain/Models/Vehiculo.cs
+++ Parqueadero.Core/Domain/Models/Vehiculo.cs
@@ -5,12 +5,12 @@ namespace Parqueadero.Core.Domain
 {
     public class Vehiculo
     {
-        public VehicleTypes Tipo { get; private set; }
+        public VehicleType Tipo { get; private set; }
         public int Cilindraje { get; private set; }
         public string Placa { get; private set; }
         public DateTime FechaIngreso { get; private set; }
 
-        public Vehiculo(VehicleTypes tipo, int cilindraje, string placa, DateTime fechaIngreso)
+        public Vehiculo(VehicleType tipo, int cilindraje, string placa, DateTime fechaIngreso)
         {
             Tipo = tipo;
             Cilindraje = cilindraje;
