da8db07012edad5c430b998dd88eb98e3f380a24
Felipe Arcos <pipearcos221@gmail.com>
2019-11-06 11:51:04 -0500

    Se modifica el UnitTest para el tiempo de estadia

--- Parqueadero.Core.Test/TiempoDeEstadiaTest.cs
+++ Parqueadero.Core.Test/TiempoDeEstadiaTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Parqueadero.Core.Domain.Interfaces;
+using Parqueadero.Core.Domain;
+using Parqueadero.Core.Domain.Enumerations;
 using Xunit;
 
 namespace Parqueadero.Core.Test
@@ -10,22 +11,31 @@ namespace Parqueadero.Core.Test
         public void ObtenerNumeroDeDiasDeEstadiaTest()
         {
             // arrange
-            DateTime fechaIngreso = DateTime.Now.AddDays(-2);
-            TiempoDeEstadiaImpl tiempoDeEstadia = new TiempoDeEstadiaImpl();
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 125, "CBD432", DateTime.Now.AddDays(-2));
             // act
-            int diasACobrar = tiempoDeEstadia.ObtenerNumeroDeDiasDeEstadia(fechaIngreso);
+            int diasACobrar = vehiculo.ObtenerNumeroDeDiasDeEstadia();
             // assert
             Assert.Equal(2, diasACobrar);
+        }
+
+        [Fact(DisplayName = "cobro de dia extra por exceso de 9 horas")]
+        public void ObtenerNumeroDeDiasDeEstadiaConDiaExtraTest()
+        {
+            // arrange
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 125, "CBD432", DateTime.Now.AddDays(-2).AddHours(-10));
+            // act
+            int diasACobrar = vehiculo.ObtenerNumeroDeDiasDeEstadia();
+            // assert
+            Assert.Equal(3, diasACobrar);
         }
 
         [Fact]
         public void ObtenerNumeroDeHorasDeEstadiaTest()
         {
             // arrange
-            DateTime fechaIngreso = DateTime.Now.AddDays(-2).AddHours(-8);
-            TiempoDeEstadiaImpl tiempoDeEstadia = new TiempoDeEstadiaImpl();
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 125, "CBD432", DateTime.Now.AddDays(-2).AddHours(-8));
             // act
-            int horasACobrar = tiempoDeEstadia.ObtenerNumeroDeHorasDeEstadia(fechaIngreso);
+            int horasACobrar = vehiculo.ObtenerNumeroDeHorasDeEstadia();
             // assert
             Assert.Equal(8, horasACobrar);
         }
