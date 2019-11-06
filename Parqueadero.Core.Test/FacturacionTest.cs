3b593fac356202535059bce4a22de5fde4b57098
Felipe Arcos <pipearcos221@gmail.com>
2019-11-06 11:50:43 -0500

    Se modifica el test para la facturacion

--- Parqueadero.Core.Test/FacturacionTest.cs
+++ Parqueadero.Core.Test/FacturacionTest.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using Parqueadero.Core.Domain;
+using Parqueadero.Core.Domain.Enumerations;
 using Xunit;
 
 namespace Parqueadero.Core.Test
 {
     public class FacturacionTest
     {
-        [Fact]
-        public void CalcularPrecioAPagarTest() {
+        [Theory]
+        [InlineData(VehicleType.Carro, 1600, 1, 1)]
+        [InlineData(VehicleType.Moto, 200, 2, 2)]
+        [InlineData(VehicleType.Moto, 1000, 1, 6)]
+        public void CalcularPrecioAPagarTest(VehicleType tipoVehiculo,int cilindraje, int numeroDiasACobrar, int numeroHorasACobrar) {
             // arrange
-            FacturacionImpl facturacion = new FacturacionImpl();
+            Vehiculo vehiculo = new Vehiculo(tipoVehiculo, cilindraje, "CBD432", DateTime.Now.AddDays(-numeroDiasACobrar).AddHours(-numeroHorasACobrar));
             // act
-            int precioAPagar = facturacion.CalcularPrecioAPagar(1, 1, 1, 1600);
+            int precioAPagar = vehiculo.CalcularPrecioAPagar(numeroDiasACobrar, numeroHorasACobrar);
             // assert
             Assert.Equal(9000, precioAPagar);
         }
