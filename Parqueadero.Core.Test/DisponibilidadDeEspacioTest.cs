56397bd1632263fdac4caa60971b8eb93c62b766
Felipe Arcos <pipearcos221@gmail.com>
2019-11-06 11:51:19 -0500

    se modifica el UnitTest para la disponibilidad de espacios

--- Parqueadero.Core.Test/DisponibilidadDeEspacioTest.cs
+++ Parqueadero.Core.Test/DisponibilidadDeEspacioTest.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using Parqueadero.Core.Domain.Interfaces;
+using Parqueadero.Core.Domain;
+using Parqueadero.Core.Domain.Enumerations;
 using Xunit;
 namespace Parqueadero.Core.Test
 {
     public class DisponibilidadDeEspacioTest
     {
-        [Fact]
-        public void VerificarDisponibilidadDeEspacioDeParqueoTest()
+        [Theory]
+        [InlineData(VehicleType.Carro, 20, 10)]
+        [InlineData(VehicleType.Carro, 19, 10)]
+        [InlineData(VehicleType.Moto, 20, 10)]
+        [InlineData(VehicleType.Moto, 20, 9)]
+        public void VerificarDisponibilidadDeEspacioDeParqueoTest(VehicleType tipo, int numeroDeCarrosEnParqueadero, int numeroDeMotosEnParqueadero)
         {
             // arrange
-            DisponibilidadDeEspacioImpl disponibilidad = new DisponibilidadDeEspacioImpl();
+            Vehiculo vehiculo = new Vehiculo(tipo, 125, "CBD432", DateTime.Now);
             // act
-            bool respuesta = disponibilidad.VerificarDisponibilidadDeEspacioDeParqueo(19, 10, 1);
+            bool respuesta = vehiculo.VerificarDisponibilidadDeEspacioLibreEnParqueadero(numeroDeCarrosEnParqueadero, numeroDeMotosEnParqueadero);
             // assert
             Assert.True(respuesta);
         }
