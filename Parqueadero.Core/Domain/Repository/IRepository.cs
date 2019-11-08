73253d012ec97f7f5bd2ecb38d86d6aa3647a1aa
Felipe Arcos <pipearcos221@gmail.com>
2019-11-08 08:19:29 -0500

    se crea las interfaces de repository

---
+++ Parqueadero.Core/Domain/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Parqueadero.Core.Data.Entity;
+using Parqueadero.Core.Domain.Enumerations;
+
+namespace Parqueadero.Core.Domain.Repository
+{
+    public interface IRepository
+    {
+        void RegistrarVehiculo(VehiculoDB vehiculo);
+
+        List<VehiculoDB> ListarVehiculosPorTipo(int tipo);
+
+        void EliminarVehiculoPorPlaca(string placa);
+    }
+}
