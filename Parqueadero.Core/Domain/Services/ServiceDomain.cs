6dd7c85afa660c684b55c52bddf813369f57bb79
Felipe Arcos <pipearcos221@gmail.com>
2019-11-12 07:55:56 -0500

    se crea la clase serviceDomain para orquestar los servicios

---
+++ Parqueadero.Core/Domain/Services/ServiceDomain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Parqueadero.Core.Domain.Services
+{
+    public class ServiceDomain
+    {
+        public ServiceDomain()
+        {
+        }
+
+        public void SolicitarIngresoDeVehiculo() {
+           
+        }
+
+    }
+}
