09c60468456000d733fb7128d9b0ad1826c1a66c
Felipe Arcos <pipearcos221@gmail.com>
2019-11-12 14:49:48 -0500

    se crea el servicio para realizar el ingreso de un vehiculo al parqueadero

--- Parqueadero.Core/Domain/Services/ServiceDomain.cs
+++ Parqueadero.Core/Domain/Services/ServiceDomain.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Parqueadero.Core.Data;
+using Parqueadero.Core.Data.Entity;
+using Parqueadero.Core.Domain.Enumerations;
+using Parqueadero.Core.Domain.Exceptions;
+using Parqueadero.Core.Domain.Repository;
+
 namespace Parqueadero.Core.Domain.Services
 {
     public class ServiceDomain
@@ -7,8 +16,43 @@ namespace Parqueadero.Core.Domain.Services
         {
         }
 
-        public void SolicitarIngresoDeVehiculo() {
-           
+        public void RegistrarIngresoDeVehiculo(Vehiculo vehiculo)
+        {
+            IRepository accessData = new VehiculoRepositoryImpl();
+            int numeroDeCarrosEnParqueadero;
+            int numeroDeMotosEnParqueadero;
+
+            var listaDeCarros = Task.Run(() => accessData.ListarVehiculosPorTipo((int)VehicleType.Carro));
+            var listaDeMotos = Task.Run(() => accessData.ListarVehiculosPorTipo((int)VehicleType.Moto));
+
+            numeroDeCarrosEnParqueadero = listaDeCarros.Result.Count;
+            numeroDeMotosEnParqueadero = listaDeMotos.Result.Count;
+
+            bool disponibilidadDeEspacio = vehiculo.VerificarDisponibilidadDeEspacioLibreEnParqueadero(numeroDeCarrosEnParqueadero, numeroDeMotosEnParqueadero);
+            if (disponibilidadDeEspacio is false)
+            {
+                throw new ParkingAccessException("El parqueadero no posee espacios disponibles");
+            }
+
+            bool autorizacionDeIngreso = vehiculo.VerificarAutorizacionDeAccesoAlParqueadero();
+            if (autorizacionDeIngreso is false)
+            {
+                throw new ParkingAccessException("Usted no esta autorizado para ingresar");
+            }
+
+            Task.Run(() =>
+            {
+                VehiculoDB vehiculoAIngresar = new VehiculoDB
+                {
+                    Tipo = (int)vehiculo.Tipo,
+                    Cilindraje = vehiculo.Cilindraje,
+                    Placa = vehiculo.Placa,
+                    FechaIngreso = vehiculo.FechaIngreso.ToString()
+                };
+
+                accessData.RegistrarVehiculo(vehiculoAIngresar);
+            }
+            );
         }
 
     }
