7d9741c1af604548ddd2dac1596a58a28597242f
Felipe Arcos <pipearcos221@gmail.com>
2019-11-12 14:49:21 -0500

    Se crea la excepcion de acceso al parqueadero

---
+++ Parqueadero.Core/Domain/Exceptions/ParkingAccessException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Parqueadero.Core.Domain.Exceptions
+{
+    [Serializable]
+    public class ParkingAccessException : Exception
+    {
+        public ParkingAccessException() { }
+
+        public ParkingAccessException(string message) : base(message)
+        {
+        }
+
+    }
+}
