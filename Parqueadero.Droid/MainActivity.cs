dd217c01c5ee2bb18fc4a62c2c15ec22a25434dc
Felipe Arcos <pipearcos221@gmail.com>
2019-11-13 15:28:31 -0500

    Se crea layout y Activity de la pantalla principal, en el activity se recupera los Views de los botones del layout y se realiza navegacion o otros activities

--- Parqueadero.Droid/MainActivity.cs
+++ Parqueadero.Droid/MainActivity.cs
@@ -1,19 +1,50 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
 using Android.Widget;
+using Android.Content;
 
 namespace Parqueadero.Droid
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button btnIngresar;
+        Button btnFacturar;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            btnIngresar = FindViewById<Button>(Resource.Id.btn_ingresar);
+            btnFacturar = FindViewById<Button>(Resource.Id.btn_facturar);
         }
-      
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            btnIngresar.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(FormActivity));
+                StartActivity(intent);
+                //Toast.MakeText(Application.Context, "sisisi", ToastLength.Long).Show();
+            };
+            btnFacturar.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(PayActivity));
+            };
+            //Intent intent = new Intent(this, AddActivity.class);
+            //  startActivity(intent);
+        }
+
+        //public void OnClick(View V)
+        //{
+        //    Toast.MakeText(Application.Context, "sisisi", ToastLength.Long).Show();
+        //}
+
+
     }
+
 }
+
