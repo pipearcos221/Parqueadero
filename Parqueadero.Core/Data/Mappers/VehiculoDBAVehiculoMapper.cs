a9051b070df16a54a4b9295663f62e5c4b52d3d7
Felipe Arcos <pipearcos221@gmail.com>
2019-11-14 16:48:47 -0500

    se crea mapper para convertir la entidad vehiculoDB al modelo Vehiculo

---
+++ Parqueadero.Core/Data/Mappers/VehiculoDBAVehiculoMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Parqueadero.Core.Data.Entity;
+using Parqueadero.Core.Domain;
+using Parqueadero.Core.Domain.Enumerations;
+
+namespace Parqueadero.Core.Data.Mappers
+{
+    public class VehiculoDBAVehiculoMapper
+    {
+        public Vehiculo Mapear(VehiculoDB vehiculodb)
+        {
+            return new Vehiculo(
+                tipo: (VehicleType)vehiculodb.Tipo,
+                cilindraje: vehiculodb.Cilindraje,
+                placa: vehiculodb.Placa,
+                fechaIngreso: Convert.ToDateTime(vehiculodb.FechaIngreso)
+                );
+        }
+    }
+}
