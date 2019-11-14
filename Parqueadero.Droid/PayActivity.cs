1518f2c9277d2cd6bf60d5fc63fbbadb280d7061
Felipe Arcos <pipearcos221@gmail.com>
2019-11-14 07:57:02 -0500

    se cambia la clase de la que heredan las actividades de Activity a AppCompatActivity

--- Parqueadero.Droid/PayActivity.cs
+++ Parqueadero.Droid/PayActivity.cs
@@ -1,20 +1,12 @@
 ﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using Android.Support.V7.App;
 
 namespace Parqueadero.Droid
 {
     [Activity(Label = "PayActivity", Theme = "@style/AppTheme")]
-    public class PayActivity : Activity
+    public class PayActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
