b363874f1a1f0a527a5899b00f294c9af11278a1
Felipe Arcos <pipearcos221@gmail.com>
2019-11-14 07:56:07 -0500

    se agrega logica en FormActivity para detectar el click en el boton finalizar

--- Parqueadero.Droid/FormActivity.cs
+++ Parqueadero.Droid/FormActivity.cs
@@ -1,26 +1,45 @@
 ﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using Parqueadero.Core.Domain.Services;
 
 namespace Parqueadero.Droid
 {
     [Activity(Label = "FormActivity", Theme = "@style/AppTheme")]
-    public class FormActivity : Activity
+    public class FormActivity : AppCompatActivity
     {
+        ServiceDomain services = new ServiceDomain();
+        Button btnRegistrarse;
+        RadioGroup tipoVehiculo;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_form);
             Title = "Registro";
+            btnRegistrarse = FindViewById<Button>(Resource.Id.btn_finalizar_registro);
+            tipoVehiculo = FindViewById<RadioGroup>(Resource.Id.radiogroup_tipo);
+
+            btnRegistrarse.Click += BtnRegistrarse_Click;
         }
+
+        
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            
+        }
+
+        private void BtnRegistrarse_Click(object sender, System.EventArgs e)
+        {
+            var x = tipoVehiculo.Selected;
+            Toast.MakeText(Application.Context, $"Valor: {x}", ToastLength.Short).Show();
+        }
+
+
     }
 }
