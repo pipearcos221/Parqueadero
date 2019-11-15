549db68f20f8363f7e97e0d404d5da1a0ba354b0
Felipe Arcos <pipearcos221@gmail.com>
2019-11-15 13:51:11 -0500

    se cambia el nombre a la clase que implementa la interfaz VehiculoRepository

---
+++ Parqueadero.Core/Data/RepositoryImplementations/VehiculoRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Parqueadero.Core.Data.Entity;
+using Parqueadero.Core.Data.Mappers;
+using Parqueadero.Core.Domain;
+using Parqueadero.Core.Domain.Repository;
+using Realms;
+
+namespace Parqueadero.Core.Data
+{
+    public class VehiculoRepository : IVehiculoRepository
+    {
+        private Realm realm = Realm.GetInstance();
+        private VehiculoAVehiculoDBMapper vehiculoAVehiculoDBMapper = new VehiculoAVehiculoDBMapper();
+        private VehiculoDBAVehiculoMapper vehiculoDBAVehiculoMapper = new VehiculoDBAVehiculoMapper();
+
+        public void RegistrarVehiculo(Vehiculo vehiculo)
+        {
+            var vehiculoDB = vehiculoAVehiculoDBMapper.Mapear(vehiculo);
+            realm.Write(() =>
+            {
+                realm.Add(vehiculoDB);
+            });
+        }
+
+        public List<Vehiculo> ListarVehiculosPorTipo(int tipo)
+        {
+            List<VehiculoDB> lista = realm.All<VehiculoDB>().Where(v => v.Tipo == tipo).ToList();
+            List<Vehiculo> listaVehiculos = new List<Vehiculo>();
+            foreach (VehiculoDB element in lista)
+            {
+                Vehiculo vehiculo = vehiculoDBAVehiculoMapper.Mapear(element);
+                listaVehiculos.Add(vehiculo);
+            }
+            return listaVehiculos;
+        }
+
+        public void EliminarVehiculoPorPlaca(string placa)
+        {
+            var vehiculoAEliminar = realm.All<VehiculoDB>().First(v => v.Placa == placa);
+            using var transaction = realm.BeginWrite();
+            realm.Remove(vehiculoAEliminar);
+            transaction.Commit();
+        }
+    }
+}
