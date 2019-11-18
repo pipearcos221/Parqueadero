912974fe6a5653438cb97d2c9712b3f39b1eeb1d
Edgar Felipe Arcos Trejo - Ceiba Software <edgar.arcos@ceiba.com.co>
2019-11-18 16:28:52 -0500

    se modifica los tests de facturacion para que el nombre sea mas descriptivo y para que todas las pruebas corran

--- Parqueadero.Core.Test/FacturacionTest.cs
+++ Parqueadero.Core.Test/FacturacionTest.cs
@@ -8,30 +8,93 @@ namespace Parqueadero.Core.Test
     public class FacturacionTest
     {
         [Fact]
-        public void CalcularPrecioAPagarParaUnCarroConEstadiaDeUnDia() {
+        public void CalcularPrecioAPagarParaUnCarroConEstadiaDeUnDia()
+        {
             // Arrange
             Vehiculo vehiculo = new Vehiculo(VehicleType.Carro, 1600, "BLA433", DateTime.Now.AddDays(-1));
+            int numeroDiasACobrar = 1;
+            int numeroHorasACobrar = 0;
 
             // Act
             int precioAPagar = vehiculo.CalcularPrecioAPagar(numeroDiasACobrar, numeroHorasACobrar);
 
             // Assert
-            Assert.Equal(9000, precioAPagar);
+            Assert.Equal(8000, precioAPagar);
         }
 
-        [Theory]
-        [InlineData(VehicleType.Carro, 1600, 1, 1)]
-        [InlineData(VehicleType.Moto, 200, 2, 2)]
-        [InlineData(VehicleType.Moto, 1000, 1, 6)]
-        public void CalcularPrecioAPagarParaUnCarroQueNoExcedeElNumeroDeHorasParaCobroPorDia(VehicleType tipoVehiculo,int cilindraje, int numeroDiasACobrar, int numeroHorasACobrar) {
+        [Fact]
+        public void CalcularPrecioAPagarParaUnCarroConEstadiaDeUnaHora()
+        {
             // Arrange
-            Vehiculo vehiculo = new Vehiculo(tipoVehiculo, cilindraje, "CBD432", DateTime.Now.AddDays(-numeroDiasACobrar).AddHours(-numeroHorasACobrar));
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Carro, 1600, "BLA433", DateTime.Now.AddHours(-1));
+            int numeroDiasACobrar = 0;
+            int numeroHorasACobrar = 1;
 
             // Act
             int precioAPagar = vehiculo.CalcularPrecioAPagar(numeroDiasACobrar, numeroHorasACobrar);
 
             // Assert
-            Assert.Equal(9000, precioAPagar);
+            Assert.Equal(1000, precioAPagar);
+        }
+
+        [Fact]
+        public void CalcularPrecioAPagarParaUnaMotoConEstadiaDeUnDiaSinExcedentePorCilindraje()
+        {
+            // Arrange
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 200, "Pk205R", DateTime.Now.AddDays(-1));
+            int numeroDiasACobrar = 1;
+            int numeroHorasACobrar = 0;
+
+            // Act
+            int precioAPagar = vehiculo.CalcularPrecioAPagar(numeroDiasACobrar, numeroHorasACobrar);
+
+            // Assert
+            Assert.Equal(4000, precioAPagar);
+        }
+
+        [Fact]
+        public void CalcularPrecioAPagarParaUnaMotoConEstadiaDeUnaHoraSinExcedentePorCilindraje()
+        {
+            // Arrange
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 200, "Pk205R", DateTime.Now.AddHours(-1));
+            int numeroDiasACobrar = 0;
+            int numeroHorasACobrar = 1;
+
+            // Act
+            int precioAPagar = vehiculo.CalcularPrecioAPagar(numeroDiasACobrar, numeroHorasACobrar);
+
+            // Assert
+            Assert.Equal(500, precioAPagar);
+        }
+
+        [Fact]
+        public void CalcularPrecioAPagarParaUnaMotoConEstadiaDeUnDiaConExcedentePorCilindraje()
+        {
+            // Arrange
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 600, "Pk205R", DateTime.Now.AddDays(-1));
+            int numeroDiasACobrar = 1;
+            int numeroHorasACobrar = 0;
+
+            // Act
+            int precioAPagar = vehiculo.CalcularPrecioAPagar(numeroDiasACobrar, numeroHorasACobrar);
+
+            // Assert
+            Assert.Equal(6000, precioAPagar);
+        }
+
+        [Fact]
+        public void CalcularPrecioAPagarParaUnaMotoConEstadiaDeUnaHoraConExcedentePorCilindraje()
+        {
+            // Arrange
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 600, "Pk205R", DateTime.Now.AddHours(-1));
+            int numeroDiasACobrar = 0;
+            int numeroHorasACobrar = 1;
+
+            // Act
+            int precioAPagar = vehiculo.CalcularPrecioAPagar(numeroDiasACobrar, numeroHorasACobrar);
+
+            // Assert
+            Assert.Equal(2500, precioAPagar);
         }
     }
 }
