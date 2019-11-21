810c64dc916bc58036f08ccb7de0d037ae430a88
Edgar Felipe Arcos Trejo - Ceiba Software <edgar.arcos@ceiba.com.co>
2019-11-21 16:51:10 -0500

    Se modifican los test para el tiempo de estadia para que sean mas descriotivos y todos pasen

--- Parqueadero.Core.Test/TiempoDeEstadiaTest.cs
+++ Parqueadero.Core.Test/TiempoDeEstadiaTest.cs
@@ -8,36 +8,55 @@ namespace Parqueadero.Core.Test
     public class TiempoDeEstadiaTest
     {
         [Fact]
-        public void ObtenerNumeroDeDiasDeEstadiaTest()
+        public void ObtenerNumeroDeDiasACobrarCuandoNoExcedeLasHorasLimite()
         {
-            // arrange
-            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 125, "CBD432", DateTime.Now.AddDays(-2));
-            // act
+            // Arrange
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Carro, 1200, "CBD432", DateTime.Now.AddDays(-2));
+
+            // Act
             int diasACobrar = vehiculo.ObtenerNumeroDeDiasDeEstadia();
-            // assert
+
+            // Assert
             Assert.Equal(2, diasACobrar);
         }
 
-        [Fact(DisplayName = "cobro de dia extra por exceso de 9 horas")]
-        public void ObtenerNumeroDeDiasDeEstadiaConDiaExtraTest()
+        [Fact]
+        public void ObtenerNumeroDeDiasACobrarCuandoSeExcedeDeLasHorasLimite()
         {
-            // arrange
-            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 125, "CBD432", DateTime.Now.AddDays(-2).AddHours(-10));
-            // act
+            // Arrange
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Carro, 1200, "CBD432", DateTime.Now.AddHours(-10));
+
+            // Act
             int diasACobrar = vehiculo.ObtenerNumeroDeDiasDeEstadia();
-            // assert
-            Assert.Equal(3, diasACobrar);
+
+            // Assert
+            Assert.Equal(1, diasACobrar);
         }
 
         [Fact]
-        public void ObtenerNumeroDeHorasDeEstadiaTest()
+        public void ObtenerNumeroDeHorasDeEstadiaACobrarCuandoNoExcedeLasHorasLimite()
         {
-            // arrange
-            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 125, "CBD432", DateTime.Now.AddDays(-2).AddHours(-8));
-            // act
+            // Arrange
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 125, "CBD432", DateTime.Now.AddHours(-6));
+
+            // Act
             int horasACobrar = vehiculo.ObtenerNumeroDeHorasDeEstadia();
-            // assert
-            Assert.Equal(8, horasACobrar);
+
+            // Assert
+            Assert.Equal(6, horasACobrar);
+        }
+
+        [Fact]
+        public void ObtenerNumeroDeHorasDeEstadiaACobrarCuandoSeExcedeLasHorasLimite()
+        {
+            // Arrange
+            Vehiculo vehiculo = new Vehiculo(VehicleType.Moto, 125, "CBD432", DateTime.Now.AddHours(-10));
+
+            // Act
+            int horasACobrar = vehiculo.ObtenerNumeroDeHorasDeEstadia();
+
+            // Assert
+            Assert.Equal(0, horasACobrar);
         }
     }
 }
