3b7e8cdc7fd924a809b2d178b9791bd13f6aa250
Edgar Felipe Arcos Trejo - Ceiba Software <edgar.arcos@ceiba.com.co>
2019-12-01 17:49:35 -0500

    Se modifica el tipo de variable para la propiedad Id

--- Parqueadero.Core/Data/Entity/VehiculoDB.cs
+++ Parqueadero.Core/Data/Entity/VehiculoDB.cs
@@ -6,7 +6,7 @@ namespace Parqueadero.Core.Data.Entity
     public class VehiculoDB : RealmObject
     {
         [PrimaryKey]
-        public long Id { get; set; } = Convert.ToInt64(Guid.NewGuid().ToString());
+        public String Id { get; set; } = Guid.NewGuid().ToString();
         public int Tipo { get; set; }
         public int Cilindraje { get; set; }
         public string Placa { get; set; }
