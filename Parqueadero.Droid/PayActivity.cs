a52bbd4d79fd72c7688168d66df1a2439ef29f77
Edgar Felipe Arcos Trejo - Ceiba Software <edgar.arcos@ceiba.com.co>
2019-12-02 02:01:34 -0500

    se declaran las variables y views

--- Parqueadero.Droid/PayActivity.cs
+++ Parqueadero.Droid/PayActivity.cs
@@ -7,6 +7,17 @@ namespace Parqueadero.Droid
     [Activity(Label = "PayActivity", Theme = "@style/AppTheme")]
     public class PayActivity : AppCompatActivity
     {
+        #region Declarar variables
+        ServiceDomain services = new ServiceDomain();
+        TextInputLayout textInputLayoutPlacaFacturar;
+        TextInputEditText textInputEditTextPlacaFacturar;
+        Button btnBuscar;
+        Button btnPagar;
+        TextView precio;
+        Vehiculo vehiculoDeSalida = new Vehiculo();
+        #endregion
+
+        #region Ciclo de vida
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
