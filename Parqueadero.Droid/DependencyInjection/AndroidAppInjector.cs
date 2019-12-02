8a1d599939929615be876f4dfdb28a9b6295ee34
Edgar Felipe Arcos Trejo - Ceiba Software <edgar.arcos@ceiba.com.co>
2019-12-02 09:20:59 -0500

    Se crea archivos Injector y Registry para la inyección de dependencias

---
+++ Parqueadero.Droid/DependencyInjection/AndroidAppInjector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using Unity; 
+
+namespace Parqueadero.Droid.DependencyInjection
+{
+   [Application(Label = "@string/app_name")]
+    public class AndroidAppInjector : Application
+    {
+        public static AndroidAppInjector Instance { get; private set; }
+        static UnityContainer DiContainer { get; set; }
+        public AndroidAppInjector(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer)
+        {
+            Instance = this;
+            DiContainer = new UnityContainer();
+            AndroidDependencyRegistry.RegisterTypes(DiContainer);
+        }
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            AndroidEnvironment.UnhandledExceptionRaiser += AndroidEnvironmentUnhandledExceptionRaiser;
+        }
+        public static TRequest Resolve<TRequest>()
+        {
+            if (DiContainer == null)
+            {
+                throw new InvalidOperationException("El contenedor de inyecciones aún no esta listo");
+            }
+            return DiContainer.Resolve<TRequest>();
+        }
+        void AndroidEnvironmentUnhandledExceptionRaiser(object sender, RaiseThrowableEventArgs e)
+        {   
+            //Log.Error("", "", + e.Exception);
+        }
+    }
+}
---
+++ Parqueadero.Droid/DependencyInjection/AndroidDependencyRegistry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Parqueadero.Core.Data;
+using Parqueadero.Core.Domain.Repository;
+using Unity;
+
+namespace Parqueadero.Droid.DependencyInjection
+{
+    public static class AndroidDependencyRegistry
+    {
+        public static void RegisterTypes(IUnityContainer unityContainer)
+        {
+            unityContainer.RegisterType<IVehiculoRepository, VehiculoRepository>();
+        }
+    }
+}
